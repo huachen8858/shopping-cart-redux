361ee55703a0835919d966bc777afe52f1356e67
huachen8858 <huachen8858@gmail.com>
2024-03-03 19:55:33 +0800

    處理購物車相關的狀態

--- store/slices/cart-slice.js
+++ store/slices/cart-slice.js
@@ -6,10 +6,17 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [];
 
+// 處理購物車相關的狀態，一個slice可以包含多個reducer函式，每個函式都負責處理特定的動作類型
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart(state, action) {
+      state.push(action.payload);
+    },
+  },
 });
+
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
