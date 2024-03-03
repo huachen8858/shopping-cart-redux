e15481fc56015f170222a262a5a2e0d4db75b2e8
huachen8858 <huachen8858@gmail.com>
2024-03-03 19:39:44 +0800

    product-tile

--- components/product-tile/index.js
+++ components/product-tile/index.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
 
-export default function ProductTile({product}) {
+export default function ProductTile({ product }) {
   return (
-    <div>ProductTile</div>
-  )
+    <div>
+      <div className="group flex flex-col items-center border-2 border-blue-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl">
+        <div className="h-[180px]">
+          <img
+            src={product?.image}
+            alt={product?.title}
+            className="object-cover h-full w-full"
+          />
+        </div>
+        <div>
+          <h1 className="w-40 truncate mt-3 text-gray-700 font-bold text-lg">
+            {product?.title}
+          </h1>
+        </div>
+        <div className="flex items-center justify-center w-full mt-5">
+          <button className="bg-blue-950 text-white rounded-lg font-bold px-8 py-2">Add to cart</button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function HomePage() {
             />
           </div>
         ) : (
-          <div className="min-h-[80vh] grid sm:grid-col-2 md:grid-cols-3 space-x-5 space-y-10 lg:grid-cols-4 max-w-6xl mx-auto p-3">
+          <div className="min-h-[80vh] grid sm:grid-col-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl mx-auto p-3">
           {
             products && products.length ? products.map((productItem, index) => {
               return (
