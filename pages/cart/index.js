23d22e3f72af7d694aec08c4770dcbade5ee6086
huachen8858 <huachen8858@gmail.com>
2024-03-03 20:37:59 +0800

    Cart Summary

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Header from "@/components/layout/header";
 import { useSelector } from "react-redux";
 import Link from "next/link";
+import CartTile from "@/components/cart-tile";
 
 export default function CartPage() {
   const [totalCart, setTotalCart] = useState(0); // 購物車金額
@@ -18,14 +19,39 @@ export default function CartPage() {
   return (
     <>
       <Header />
-      {cart && cart.length ? <div></div> : <div className="min-h-[80vh] flex flex-col justify-center items-center">
-        <h1 className="text-gray-800 font-bold text-xl mb-2">Your cart is empty</h1>
-        <Link href="/">
-          <button className="bg-blue-950 text-white rounded-lg font-bold px-8 py-2">
-            SHOP NOW
-          </button>
-        </Link>
-      </div>}
+      {cart && cart.length ? (
+        <>
+        <div className="min-h-[80vh] grid md:grid-cols-2 max-w-6xl mx-auto">
+          <div className="flex flex-col justify-center items-center p-3">
+            {cart.map((cartItem, index) => {
+              return <CartTile cartItem={cartItem} key={index}/>
+            })}
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-end p-5 space-y-5 mt-14">
+          <h1 className="font-bold text-lg text-blue-800">Your Cart Summary</h1>
+          <p>
+            <span className="text-gray-800 font-bold">Total Item</span>
+            <span>: {cart.length}</span>
+          </p>
+          <p>
+          <span className="text-gray-800 font-bold">Total Amount</span>
+            <span>: {totalCart}</span>
+          </p>
+        </div>
+        </>
+      ) : (
+        <div className="min-h-[80vh] flex flex-col justify-center items-center">
+          <h1 className="text-gray-800 font-bold text-xl mb-2">
+            Your cart is empty
+          </h1>
+          <Link href="/">
+            <button className="bg-blue-950 text-white rounded-lg font-bold px-8 py-2">
+              SHOP NOW
+            </button>
+          </Link>
+        </div>
+      )}
     </>
   );
 }
