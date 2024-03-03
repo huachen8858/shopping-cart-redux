82d3a725ad9b181ba1006c2e1d40a879be7fab76
huachen8858 <huachen8858@gmail.com>
2024-03-03 19:29:19 +0800

    fetch products

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,55 @@
 import Header from "@/components/layout/header";
-import Link from "next/link";
+import ProductTile from "@/components/product-tile";
+import { useEffect, useState } from "react";
+import { Circles } from "react-loader-spinner";
 
-export default function Home() {
+export default function HomePage() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchListOfProducts = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      if (data) {
+        setLoading(false);
+        setProducts(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchListOfProducts();
+  }, []);
+
   return (
     <div>
       <Header />
-      <div>Home</div>
+      <div>
+        {loading ? (
+          <div className="min-h-screen w-full flex justify-center items-center">
+            <Circles
+              visible={true}
+              height={"120"}
+              width={"120"}
+              color="rgb(196,196,196)"
+            />
+          </div>
+        ) : (
+          <div className="min-h-[80vh] grid sm:grid-col-2 md:grid-cols-3 space-x-5 space-y-10 lg:grid-cols-4 max-w-6xl mx-auto p-3">
+          {
+            products && products.length ? products.map((productItem, index) => {
+              return (
+                <ProductTile key={index} product={productItem}/>
+              )
+            }) : null
+          }
+          </div>
+        )}
+      </div>
     </div>
   );
 }
